b9ae6772fec9b6a4a0b42af887be30e9c8e0976f
Rajath <rajath.hb@gmail.com>
2025-03-29 11:39:03 +0530

    Updated Learn047.java with latest changes

--- src/rajathLearning/Oops/Learn047.java
+++ src/rajathLearning/Oops/Learn047.java
@@ -3,8 +3,17 @@ package rajathLearning.Oops;
 public class Learn047 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		int a = sum(8,4);
+			System.out.println(a);
+		
 	}
+		
+		public static int sum(int c, int d) {
+			return c+d;
+		
+	}
+
 
 }
