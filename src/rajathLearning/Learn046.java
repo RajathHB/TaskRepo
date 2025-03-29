f5a2f6e5d9f05e6b5388e89ab747476efd66f3e1
Rajath <rajath.hb@gmail.com>
2025-03-29 11:36:47 +0530

    29th March

---
+++ src/rajathLearning/Learn046.java
@@ -0,0 +1,23 @@
+package rajathLearning;
+
+public class Learn046 {
+
+	public static void main(String[] args) {
+		
+		
+		int d = sum(3,4);
+		System.out.println(d);
+		
+		
+	}
+		
+	
+
+		
+	
+	public static int sum(int a,int b) {
+		return a+b;
+		
+	}
+	
+}
---
+++ src/rajathLearning/Oops/Learn047.java
@@ -0,0 +1,10 @@
+package rajathLearning.Oops;
+
+public class Learn047 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
